db1c1077cac0be8da7148c0b305f80262bcf8e16
Henry <mli42@bwh.harvard.edu>
2020-01-31 14:41:15 -0500

    adding templates

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home(): 
-    return "<h1>This is Home Page</h1>"
+    return render_template('index.html')
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run()
+
